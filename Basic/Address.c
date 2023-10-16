c985d198b4328a245417b6ec73b5c147441d347e
Deep-Vaghamashi <147982510+Deep-Vaghamashi@users.noreply.github.com>
2023-10-16 17:53:12 +0530

    Delete Basic directory

--- Basic/Address.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-
-void main() {
-	printf("Darshan University,Morbi Road,Rajkot\n");
-	printf("Darshan University\n");
-	printf("Morbi Road\n");
-	printf("Rajkot\n");
-}
--- Basic/F_to_C.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
- void main(){
- 	float f,c;
- 	printf("write the value of Fahrenheit:");
- 	scanf("%f",&f);
- 	c=(((f-32)*5)/9);
- 	printf("value of Celsius:""%f",c);
- }
