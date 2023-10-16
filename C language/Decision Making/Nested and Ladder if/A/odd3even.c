2ee3b8c49b9f3523bb31fc42923f37ee4c534e0f
Deep-Vaghamashi <147982510+Deep-Vaghamashi@users.noreply.github.com>
2023-10-16 18:00:49 +0530

    Add files via upload

---
+++ C language/Decision Making/Nested and Ladder if/A/odd3even.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+ void main(){
+ 	int n;
+ 	printf("Enter the value of number:");
+ 	scanf("%d",&n);
+ 	if(n%2==1){
+ 		printf("The number %d is odd.",n); 	
+	 }
+	else{
+		printf("The number %d is even.",n);
+	} 
+ }
