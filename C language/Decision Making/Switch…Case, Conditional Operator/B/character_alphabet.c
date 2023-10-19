32bc55ac2675bab5e4f9d1b8c568ac143e36f3f2
Deep-Vaghamashi <147982510+Deep-Vaghamashi@users.noreply.github.com>
2023-10-19 19:41:37 +0530

    Add files via upload

---
+++ C language/Decision Making/Switch…Case, Conditional Operator/B/character_alphabet.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+ void main(){
+ char a;
+ printf("Enter the char:");
+ scanf("%c",&a);
+ (a>='a'&&a<='z'||a>='A'&&a<='Z')?printf("This is alphabet."):printf("This is not alphabet."); 
+ }
+
