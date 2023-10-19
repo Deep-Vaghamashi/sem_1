ecc90782ef8fcaa57145d2473731d4ec438e8330
Deep-Vaghamashi <147982510+Deep-Vaghamashi@users.noreply.github.com>
2023-10-19 18:59:58 +0530

    Delete C language/Decision Making/Nested and Ladder if/C/largest2.c

--- C language/Decision Making/Nested and Ladder if/C/largest2.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
- void main(){
- 	int a,b,c;
- 	printf("Enter the three number:");
- 	scanf("%d %d %d",&a,&b,&c);
- 	(a>b)&&(a>c)?printf("%d",a):(c>b)?printf("%d",c):printf("%d",b);
- }
