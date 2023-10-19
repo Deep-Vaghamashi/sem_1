39ebc575d492ef94dfa0c798af012891750efc6a
Deep-Vaghamashi <147982510+Deep-Vaghamashi@users.noreply.github.com>
2023-10-19 19:32:57 +0530

    Add files via upload

---
+++ C language/Decision Making/Switch…Case, Conditional Operator/B/multiply_largest .c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+ void main(){
+int a,b,c,d=0;
+  printf("Enter the first number:");
+  scanf("%d",&a);
+  printf("Enter the second number:");
+  scanf("%d",&b);
+  printf("Enter the third number:");
+  scanf("%d",&c);
+  (a>b)?printf("%d",a*c):printf("%d",b*c);
+ }
+
